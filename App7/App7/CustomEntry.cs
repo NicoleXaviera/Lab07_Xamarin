b71cb9f619e2fd0e02b2b14ea81a6465402cab52
NicoleXaviera <nicole.arguedas@tecsup.edu.pe>
2023-10-20 15:37:37 -0500

    segundo commit

--- App7/App7/CustomEntry.cs
+++ App7/App7/CustomEntry.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace App7
 {
-    internal class CustomEntry
+    public class CustomEntry:Entry
     {
     }
 }
